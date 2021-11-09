493e616f230c6ba25fdf95f8793a56a10b5b9cd7
Atlanfa <iliatalgar@gmail.com>
2021-11-09 15:54:48 +0200

    basic example of minimax

---
+++ minimax.py
@@ -0,0 +1,25 @@
+from infinity import inf
+
+
+def minimax(game_field, depth, alpha, beta, maximizing_player):
+    if depth == 0 or game_field.game_over():                    # TODO game_field.game_over()
+        return static_evaluation_of_game_field(game_field)      # TODO
+
+    if maximizing_player:
+        max_evaluation = -inf
+        for position in get_all_moves(game_field):               # TODO
+            evaluation = minimax(position, depth - 1, alpha, beta, False)
+            max_evaluation = max(max_evaluation, evaluation)
+            alpha = max(alpha, evaluation)
+            if beta <= alpha:
+                break
+        return max_evaluation
+    else:
+        min_evaluation = +inf
+        for position in get_all_moves(game_field):               # TODO
+            evaluation = minimax(position, depth - 1, alpha, beta, True)
+            min_evaluation = min(min_evaluation, evaluation)
+            beta = min(beta, evaluation)
+            if beta <= alpha:
+                break
+        return min_evaluation
